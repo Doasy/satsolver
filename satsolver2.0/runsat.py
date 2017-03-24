de58a71394b389833162d319a7f32512ab18fc78
Alba Lamas <alba.v.lmas@gmail.com>
2017-03-24 10:38:47 +0100

    runsat random works

--- satsolver2.0/runsat.py
+++ satsolver2.0/runsat.py
@@ -27,29 +27,34 @@ endfor
 return "No solution found".
 '''
 
+# positions_with_zero = [i for i, j in enumerate(num_sat_lit) if j == 0]
+# x = positions_with_zero[randint(0, len(positions_with_zero)-1)]
  
 def runsat(clauses, n_vars):
     F = clauses
     literals_in_clauses = createDataStruct(F, n_vars)
+    # print "Literals: " + str(literals_in_clauses)
     while(1):
         sol = RandomSolution(n_vars)
         num_sat_lit = createDataStruct2(F, sol)
         for i in xrange(1000):
+            # print num_sat_lit
             positions_with_zero = [i for i, j in enumerate(num_sat_lit) if j == 0]
-            if (len(positions_with_zero) == 0):
+            if len(positions_with_zero) == 0:  # This could be added to the for below
                 print "s SATISFIABLE"  # Keeping it this way to simplify
                 print "v " + ' '.join(str(e) for e in sol) + " 0"
                 return sol
-
-
-            x = positions_with_zero[randint(0, len(positions_with_zero)-1)]
-            
-            if random() < 0.2:
-                to_swap = abs(F[x][randint(0, len(F[0]) - 1)])
-            else:
-                to_swap = broken(F[x], num_sat_lit,
-                                 literals_in_clauses, sol)
-            break
+            x2 = positions_with_zero[randint(0, len(positions_with_zero)-1)]
+            #print "x2:"+str(x2)
+            #for x in range(len(num_sat_lit)):
+            if num_sat_lit[x2] == 0:
+                #print "x:"+str(x)
+                if random() < 0.2:
+                    to_swap = abs(F[x2][randint(0, len(F[0]) - 1)])
+                else:
+                    to_swap = broken(F[x2], num_sat_lit,
+                                     literals_in_clauses, sol)
+                #break
 
             if sol[to_swap - 1] < 0:
                 for element in literals_in_clauses[to_swap - 1]:
@@ -62,6 +67,8 @@ def runsat(clauses, n_vars):
                 for element in literals_in_clauses[-to_swap]:
                     num_sat_lit[element] += 1
             sol[to_swap - 1] *= -1
+            
+        
 
 
 def broken(clause, num_sat_lit, literals_in_clauses, sol):
@@ -70,9 +77,9 @@ def broken(clause, num_sat_lit, literals_in_clauses, sol):
     for literal in clause:
         broken = 0
         if sol[abs(literal) - 1] < 0:
-            for element in literals_in_clauses[abs(literal) - 1]:
+            '''for element in literals_in_clauses[abs(literal) - 1]:
                 if num_sat_lit[element] == 0:
-                    broken -= 1
+                    broken -= 1'''
             for element in literals_in_clauses[-abs(literal)]:
                 if num_sat_lit[element] == 1:
                     broken += 1
@@ -80,9 +87,9 @@ def broken(clause, num_sat_lit, literals_in_clauses, sol):
             for element in literals_in_clauses[abs(literal) - 1]:
                 if num_sat_lit[element] == 1:
                     broken += 1
-            for element in literals_in_clauses[-abs(literal)]:
+            '''for element in literals_in_clauses[-abs(literal)]:
                 if num_sat_lit[element] == 0:
-                    broken -= 1
+                    broken -= 1'''
         if broken < minimum:
             minimum = broken
             to_swap = literal
