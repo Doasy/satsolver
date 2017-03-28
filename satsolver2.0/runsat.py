46cd836037c5664036d594177a5fe8ffa012eeaa
Alba Lamas <alba.v.lmas@gmail.com>
2017-03-28 15:43:20 +0200

    max flips modified

--- satsolver2.0/runsat.py
+++ satsolver2.0/runsat.py
@@ -37,22 +37,22 @@ def runsat(clauses, n_vars):
     while(1):
         sol = RandomSolution(n_vars)
         num_sat_lit = createDataStruct2(F, sol)
-        for i in xrange(1000):
+        for i in xrange(3*n_vars):
             # print num_sat_lit
             positions_with_zero = [i for i, j in enumerate(num_sat_lit) if j == 0]
             if len(positions_with_zero) == 0:  # This could be added to the for below
                 print "s SATISFIABLE"  # Keeping it this way to simplify
                 print "v " + ' '.join(str(e) for e in sol) + " 0"
                 return sol
-            x2 = positions_with_zero[randint(0, len(positions_with_zero)-1)]
-            #print "x2:"+str(x2)
+            x = positions_with_zero[randint(0, len(positions_with_zero)-1)]
+            #print "x:"+str(x)
             #for x in range(len(num_sat_lit)):
-            if num_sat_lit[x2] == 0:
+            if num_sat_lit[x] == 0:
                 #print "x:"+str(x)
-                if random() < 0.2:
-                    to_swap = abs(F[x2][randint(0, len(F[0]) - 1)])
+                if random() < 0.25:
+                    to_swap = abs(F[x][randint(0, len(F[0]) - 1)])
                 else:
-                    to_swap = broken(F[x2], num_sat_lit,
+                    to_swap = broken(F[x], num_sat_lit,
                                      literals_in_clauses, sol)
                 #break
 
